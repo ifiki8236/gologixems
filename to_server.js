f2259ea0a07779590ecf909698d01105a27061cf
Ifiok  Sunday Edemidiong <117485944+ifiki8236@users.noreply.github.com>
2023-08-29 14:25:10 -0400

    Update to_server.js

--- to_server.js
+++ to_server.js
@@ -66,7 +66,7 @@ resume.addEventListener('change', function(){
 
 function to_json(applicationData) {
     console.log(applicationData)
-    fetch('http://gologixems.com/', {
+    fetch('http://localhost:80', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
